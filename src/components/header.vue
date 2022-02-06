bdbff37433e30d57b43347c90f221f1621ab33f9
Jishnu Prasad Samal <jishnuprs20@gmail.com>
2022-02-06 20:58:04 +0530

    added navbar

---
+++ src/components/header.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+</script>
+
+<template>
+  <h1 class="font-effect-3d-float">{{msg}}</h1>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Supermercado+One&display=swap&effect=3d-float');
+h1 {
+  font-family: 'Supermercado One', cursive;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,59 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand head" href="#">Race To Future</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                &nbsp &nbsp
+            <ul class="navbar-nav me-2 mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link" href="/">Home</a>
+                </li>&nbsp &nbsp
+                <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+                </li>
+            </ul>
+            </div>
+            </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Source+Serif+4&display=swap');
+.head {
+  font-family: 'Source Serif 4', sans-serif;
+}
+
+a.nav-link
+{
+    padding-left: 10em;
+    padding-right: 15em;
+    position: relative;
+}
+
+a.nav-link:before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: #FFF;
+  visibility: hidden;
+  transform: scaleX(0);
+  transition: all 0.3s ease-in-out;
+}
+
+a.nav-link:hover:before {
+  visibility: visible;
+  transform: scaleX(1);
+}
+</style>
