b81d57a32ad02a5bd2bb1438cbb786fdc264330c
Jishnu Prasad Samal <jishnuprs20@gmail.com>
2022-02-07 19:10:26 +0530

    added partners

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HelloWorld from './components/header.vue'
 import NavBar from './components/navbar.vue'
+import Partners from './components/partners.vue'
 </script>
 
 <template>
   <NavBar />
-  <HelloWorld msg="Race To Future" />
+  <HelloWorld head="Race To Future" />
+  <Partners />
 </template>
 
 <style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -2,27 +2,39 @@
 import { ref } from 'vue'
 
 defineProps({
-  msg: String
+  head: String
 })
-
+AOS.init();
 </script>
 
 <template>
 <div class="bgimg uk-background-cover" uk-parallax="bgx: -50">
-  <h1 class="uk-heading-2xlarge uk-text-center head">{{msg}}</h1>
+  <div class='cover uk-text-center'>
+    <h1 class="uk-heading-large head" data-aos="fade-up">{{head}}</h1>
+    <p class="text" data-aos="fade-down">Are you ready to experience the future?</p>
+  </div>
 </div>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@1,500&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Supermercado+One&display=swap&effect=3d-float');
 .head {
   font-family: 'Supermercado One', cursive;
+}
+
+.cover {
+  padding: 14em 5em 15.7em 5em;
 }
 
 .bgimg {
   background-image: url('https://images.unsplash.com/photo-1495366554757-8568e69d7f80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80');
   background-repeat: no-repeat;
   background-size: cover;
-  background-attachment: scroll;
+}
+
+.text {
+  font-size: 2em;
+  font-family: 'Lora', serif;
 }
 </style>
